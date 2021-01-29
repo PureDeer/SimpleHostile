d9126d381ac368ccd54ac8a13d4e90eaec0f5b81
Bill767 <38528734+PureDeer@users.noreply.github.com>
2021-01-29 22:16:03 +0800

    Create Mark.cs
    mark that this repository is using C#

---
+++ Mark.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BL767.SimpleHostile
+{
+    public class Mark : MonoBehaviour
+    {
+        
+    }
+}
